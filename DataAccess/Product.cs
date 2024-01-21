5afae9d7f6db913460363301c7544336f53eaf43
Cenk Kıran <cenk33m@gmail.com>
2024-01-21 20:25:20 +0300

    product class was created

---
+++ DataAccess/Product.cs
@@ -0,0 +1,11 @@
+class Product 
+{
+    public int Id { get; set; }
+    public float Price { get; set; }
+    public string Type { get; set; }
+
+
+    public void Add(){}
+    public void Modify(int Id){}
+    public void Select(int Id){}
+}
