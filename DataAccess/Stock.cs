74c94db9526e7b7e52f1aa9b73f9f0a54eb2955a
Cenk Kıran <cenk33m@gmail.com>
2024-01-21 20:37:27 +0300

    stock was written function and property

---
+++ DataAccess/Stock.cs
@@ -0,0 +1,11 @@
+class Stock
+{
+    public int ProductId { get; set; }
+    public string Quantity { get; set; }
+    public int No { get; set; }
+
+
+    public void Add(){}
+    public void Modify(int ProductIdId){}
+    public void Select(int ProductIdId){}
+}
