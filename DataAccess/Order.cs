213672ea75b610944167d3b7f5c292689551d542
Cenk Kıran <cenk33m@gmail.com>
2024-01-21 20:32:02 +0300

    order was writtten function and  property by manager

--- DataAccess/Order.cs
+++ DataAccess/Order.cs
@@ -1,3 +1,14 @@
 class Order {
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public string CustomerName { get; set; }
+    public int ProductId { get; set; }
+    public int Amount { get; set; }
+    public DateTime Date { get; set; }
+
+
+    public void NewOrder(){}
+    public void Edit(int Id){}
     
+
 }
