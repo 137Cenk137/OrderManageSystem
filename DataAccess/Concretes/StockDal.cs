26ac7dcdb0fc53ce7caa1cc2fe8f843f2f9d301b
Cenk Kıran <cenk33m@gmail.com>
2024-03-16 20:43:04 +0300

    StockDal Was written

---
+++ DataAccess/Concretes/StockDal.cs
@@ -0,0 +1,10 @@
+using System.Security.Cryptography.X509Certificates;
+using OrderManageSystem.DataAccess.Abstracts;
+using OrderManageSystem.Entities.Concretes;
+
+namespace OrderManageSystem.DataAccess.Concretes;
+
+public class StockDal : IStoctDal
+{
+
+}
