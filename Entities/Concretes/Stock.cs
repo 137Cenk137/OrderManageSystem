a0c07e77f6f19c4c12434a544a159669ae4138fd
Cenk Kıran <cenk33m@gmail.com>
2024-03-22 22:51:44 +0300

    foreign key was written for stock

--- Entities/Concretes/Stock.cs
+++ Entities/Concretes/Stock.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using OrderManageSystem.Core.Entities;
 
 namespace OrderManageSystem.Entities.Concretes;
 
 public class Stock : BaseEntity<int>
 {
-   public int ProductId { get; set; }
+   
+   
    public int Quantity { get; set; }
+
+   [ForeignKey("Id")]//ProcuctId
+   public Product product { get; set; }
+
 }
